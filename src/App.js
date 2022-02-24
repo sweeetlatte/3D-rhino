dd178919cee7e5a3b61493f1722890ede8970239
sweetcapucino <thuylinh271055@gmail.com>
2022-02-24 10:26:27 +0700

    M: model rotation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./styles.css";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
-import { Environment, OrbitControls } from "@react-three/drei";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { DDSLoader } from "three-stdlib";
 import { Suspense } from "react";
+import { Physics, usePlane } from "@react-three/cannon";
 import { render, events } from "@react-three/fiber";
-
-// THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
+import {
+    Environment,
+    OrbitControls,
+    MeshReflectorMaterial,
+} from "@react-three/drei";
 
 // window.addEventListener("resize", () =>
 //     render(<mesh />, document.querySelector("canvas"), {
@@ -17,31 +20,6 @@ import { render, events } from "@react-three/fiber";
 //         size: { width: window.innerWidth / 2, height: window.innerHeight / 2 },
 //     })
 // );
-
-// const Scene = () => {
-//     const obj = useLoader(OBJLoader, "rhino.obj", (loader) => {});
-//     //   console.log(obj);
-//     return <primitive object={obj} scale={0.1} />;
-// };
-
-// export default function App() {
-//     return (
-//         <div className="App">
-//             <Canvas>
-//                 <Suspense fallback={null}>
-//                     <mesh>
-//                         <Scene />
-//                         <meshStandardMaterial />
-//                     </mesh>
-//                     <OrbitControls />
-//                     <ambientLight intensity={0.5} />
-//                     <spotLight position={[10, 15, 10]} angle={0.3} />
-//                     {/* <Environment preset="sunset" background /> */}
-//                 </Suspense>
-//             </Canvas>
-//         </div>
-//     );
-// }
 
 THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
 
@@ -53,19 +31,44 @@ const Scene = () => {
     });
 
     console.log(obj);
-    return <primitive object={obj} scale={1.4} />;
+    return <primitive object={obj} scale={1.4} rotation={[250, 0, 150]} />;
 };
+
+function Floor(props) {
+    const [ref] = usePlane(() => ({ type: "Static", ...props }));
+    return (
+        <mesh ref={ref} receiveShadow>
+            <planeGeometry args={[100, 100]} />
+            <MeshReflectorMaterial
+                color="#878790"
+                blur={[400, 400]}
+                resolution={1024}
+                mixBlur={1}
+                mixStrength={3}
+                depthScale={1}
+                minDepthThreshold={0.85}
+                metalness={0}
+                roughness={1}
+            />
+        </mesh>
+    );
+}
 
 export default function App() {
     return (
         <div className="App">
-            <Canvas>
+            <Canvas
+            // camera={{ position: [0, 0, 7],
+            // // near: 5, far: 15
+            //  }}
+            >
                 <ambientLight intensity={0.5} />
                 <spotLight position={[0, 0, 0]} angle={0.3} />
                 <Suspense fallback={null}>
                     <Scene />
                     {/* <OrbitControls /> */}
                     {/* <Environment preset="sunset" background /> */}
+                    {/* <Floor position={[0, -15, 0]} rotation={[-Math.PI / 2, 0, 0]} /> */}
                 </Suspense>
             </Canvas>
         </div>
