5666ea59dc45794b1359a7dd7c8477c190cbe125
sweetcapucino <thuylinh271055@gmail.com>
2022-03-23 16:10:58 +0700

    M: loader

--- src/App.js
+++ src/App.js
@@ -231,6 +231,11 @@ function Loader({ active, progress }) {
 
 export default function App() {
     const { active: abc, progress } = useProgress();
+    const [i, seti] = useState(0);
+    function count() {
+        if (progress === 100 && i < 2) return seti(i + 1) && false;
+        else if (i == 2) return true;
+    }
 
     const [z, setZ] = useState(1);
     const [active, setActive] = useState(0);
@@ -247,7 +252,7 @@ export default function App() {
         }, 1734);
     }
 
-    console.log("progress", progress);
+    // console.log("progress", progress);
 
     function updateDisplay() {
         if (active === 2) {
@@ -297,74 +302,74 @@ export default function App() {
                     shadow-camera-near={0.1}
                     shadow-camera-far={20}
                 /> */}
-                    {abc && 
-                    <mesh>
-                        <Html scale={1} position={[-7, 1.7, 0]}>
-                            <div className="text-[160px] 2xl:text-[216px] w-[85.1vw] 1xl:w-[80.1vw] 2xl:w-[86vw] text-white leading-[0.87] 2xl:leading-[0.9] tracking-[-4px] 2xl:tracking-normal">
-                                <div className="flex justify-between">
-                                    {/* 1.166 -> 1.400: xong dash
+                    {count() &&
+                        <mesh>
+                            <Html scale={1} position={[-7, 1.7, 0]}>
+                                <div className="text-[160px] 2xl:text-[216px] w-[85.1vw] 1xl:w-[80.1vw] 2xl:w-[86vw] text-white leading-[0.87] 2xl:leading-[0.9] tracking-[-4px] 2xl:tracking-normal">
+                                    <div className="flex justify-between">
+                                        {/* 1.166 -> 1.400: xong dash
                                 -> 1500: close xong
                                 1600: start wavy
                                 2333: i letter disapear */}
-                                    <div
-                                        className={
-                                            z === 3 ? "wavy close-reverse" : ""
-                                        }
-                                    >
-                                        <span style={{ "--i": 5 }}>R</span>
-                                        <span style={{ "--i": 1 }}>H</span>
-                                        <span style={{ "--i": 0 }}>I</span>
-                                        <span style={{ "--i": 1 }}>N</span>
-                                        <span style={{ "--i": 5 }}>O</span>
+                                        <div
+                                            className={
+                                                z === 3 ? "wavy close-reverse" : ""
+                                            }
+                                        >
+                                            <span style={{ "--i": 5 }}>R</span>
+                                            <span style={{ "--i": 1 }}>H</span>
+                                            <span style={{ "--i": 0 }}>I</span>
+                                            <span style={{ "--i": 1 }}>N</span>
+                                            <span style={{ "--i": 5 }}>O</span>
+                                        </div>
+                                        <div
+                                            className={
+                                                z === 3 ? "dash shorten-dash" : "dash"
+                                            }
+                                        >
+                                            —
+                                        </div>
+                                        <div className={z === 3 ? "wavy close" : ""} style={{ letterSpacing: "-8px" }}>
+                                            <span style={{ "--i": 5 }}>C</span>
+                                            <span style={{ "--i": 2 }}>L</span>
+                                            <span style={{ "--i": 1 }}>O</span>
+                                            <span style={{ "--i": 2 }}>S</span>
+                                            <span style={{ "--i": 5 }}>E</span>
+                                        </div>
                                     </div>
-                                    <div
-                                        className={
-                                            z === 3 ? "dash shorten-dash" : "dash"
-                                        }
-                                    >
-                                        —
-                                    </div>
-                                    <div className={z === 3 ? "wavy close" : ""} style={{ letterSpacing: "-8px" }}>
-                                        <span style={{ "--i": 5 }}>C</span>
-                                        <span style={{ "--i": 2 }}>L</span>
-                                        <span style={{ "--i": 1 }}>O</span>
-                                        <span style={{ "--i": 2 }}>S</span>
-                                        <span style={{ "--i": 5 }}>E</span>
+                                    <div className={z === 3 ? "wavy w-[86vw] 1xl:w-[81vw]" : "w-[86vw] 1xl:w-[81vw]"}>
+                                        <span style={{ "--i": 10 }}>T</span>
+                                        <span style={{ "--i": 8 }}>O</span>
+                                        <span style={{ "--i": 8 }}>&nbsp;</span>
+                                        <span style={{ "--i": 7 }}>E</span>
+                                        <span style={{ "--i": 6 }}>X</span>
+                                        <span style={{ "--i": 5 }}>T</span>
+                                        <span style={{ "--i": 3 }}>I</span>
+                                        <span style={{ "--i": 5 }}>N</span>
+                                        <span style={{ "--i": 6 }}>C</span>
+                                        <span style={{ "--i": 7 }}>T</span>
+                                        <span style={{ "--i": 5 }}>I</span>
+                                        <span style={{ "--i": 8 }}>O</span>
+                                        <span style={{ "--i": 10 }}>N</span>
                                     </div>
                                 </div>
-                                <div className={z === 3 ? "wavy w-[86vw] 1xl:w-[81vw]" : "w-[86vw] 1xl:w-[81vw]"}>
-                                    <span style={{ "--i": 10 }}>T</span>
-                                    <span style={{ "--i": 8 }}>O</span>
-                                    <span style={{ "--i": 8 }}>&nbsp;</span>
-                                    <span style={{ "--i": 7 }}>E</span>
-                                    <span style={{ "--i": 6 }}>X</span>
-                                    <span style={{ "--i": 5 }}>T</span>
-                                    <span style={{ "--i": 3 }}>I</span>
-                                    <span style={{ "--i": 5 }}>N</span>
-                                    <span style={{ "--i": 6 }}>C</span>
-                                    <span style={{ "--i": 7 }}>T</span>
-                                    <span style={{ "--i": 5 }}>I</span>
-                                    <span style={{ "--i": 8 }}>O</span>
-                                    <span style={{ "--i": 10 }}>N</span>
-                                </div>
-                            </div>
-                            <div
-                                className={z === 3 ? "wavy 2xl:mt-[12.7vh] mt-[12.1vh] 2xl:ml-[2vw] ml-[3vw] 1xl:ml-0" : "2xl:mt-[12.7vh] mt-[12.1vh] 2xl:ml-[2vw] ml-[3vw] 1xl:ml-0"}
-                            >
                                 <div
-                                    className="btn text-[22px] 2xl:text-[27px] w-max mx-auto py-[5px] px-[33px] 2xl:py-[8px] 2xl:px-[56px]"
-                                    style={{ "--i": 2 }}
-                                    onClick={() => {
-                                        setZ(3);
-                                        delayAnimate();
-                                    }}
+                                    className={z === 3 ? "wavy 2xl:mt-[12.7vh] mt-[12.1vh] 2xl:ml-[2vw] ml-[3vw] 1xl:ml-0" : "2xl:mt-[12.7vh] mt-[12.1vh] 2xl:ml-[2vw] ml-[3vw] 1xl:ml-0"}
                                 >
-                                    EXPLORE
+                                    <div
+                                        className="btn text-[22px] 2xl:text-[27px] w-max mx-auto py-[5px] px-[33px] 2xl:py-[8px] 2xl:px-[56px]"
+                                        style={{ "--i": 2 }}
+                                        onClick={() => {
+                                            setZ(3);
+                                            delayAnimate();
+                                        }}
+                                    >
+                                        EXPLORE
+                                    </div>
                                 </div>
-                            </div>
-                        </Html>
-                    </mesh>
-                    } 
+                            </Html>
+                        </mesh>
+                    }
                     <mesh>
                         <Html>
                             <div className={`${display} tetx-white`}>
@@ -642,7 +647,7 @@ export default function App() {
             </div >
 
             <Loader active={abc} progress={progress} />
-            
+
         </>
     );
 }
