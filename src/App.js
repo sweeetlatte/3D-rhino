3c4b0e8bc0ef0404dfbdf92b0dcfc47b06da279c
sweetcapucino <thuylinh271055@gmail.com>
2022-03-31 15:44:53 +0700

    M: model

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import "./styles.css";
 import "./input.css";
 import { useState, useEffect } from "react";
 import { Suspense } from "react";
-import * as THREE from "three";
-import { DDSLoader } from "three-stdlib";
-import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
-import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 import { Canvas } from "@react-three/fiber";
-import { useLoader } from "@react-three/fiber";
-import { useFrame } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
 import {
     Html,
@@ -19,116 +13,7 @@ import {
 import "./cursor.svg";
 import Loader from "./Loader/Loader";
 import Floor from "./Floor/Floor";
-
-// window.addEventListener("resize", () =>
-//     render(<mesh />, document.querySelector("canvas"), {
-//         events,
-//         size: { width: window.innerWidth / 2, height: window.innerHeight / 2 },
-//     })
-// );
-
-THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
-
-const Scene = ({ active }) => {
-    const materials = useLoader(MTLLoader, "rhino.mtl");
-    const obj = useLoader(OBJLoader, "rhino.obj", (loader) => {
-        materials.preload();
-        loader.setMaterials(materials);
-    });
-
-    useFrame(() => {
-        if (active === 2) {
-            if (obj.position.y > -0.5) {
-                obj.position.y += -0.5 / 10;
-            }
-            if (obj.position.z < 1.1) {
-                obj.position.z += 1.1 / 10;
-            }
-            if (obj.rotation.z > -1.5) {
-                obj.rotation.z -= 0.02;
-            }
-            setTimeout(() => {
-                if (obj.position.x > -1) {
-                    obj.position.x -= 1 / 45;
-                }
-            }, 1200);
-            // obj.position.y = -0.5;
-            // obj.position.z = 3.2;
-            // obj.rotation.z = -1.5;
-            // obj.position.x = -0.75; change after 3 above
-        }
-        else if (active === 3) {
-            if (obj.position.x < 0.05) {
-                obj.position.x += 0.025;
-            }
-
-            if (obj.position.z > 0.9) {
-                obj.position.z -= 0.06;
-            }
-
-            if (obj.rotation.z < -0.5) {
-                obj.rotation.z -= -0.022;
-            }
-
-            if (obj.position.y < 0) {
-                obj.position.y += 0.05;
-            }
-            // obj.position.x = 0.05;
-            // obj.position.z = 3;
-            // obj.rotation.z = -0.5;
-        }
-        else if (active === 1 && obj.position.y !== 0) {
-            if (obj.position.x < 0.05) {
-                obj.position.x += 0.001;
-            }
-
-            if (obj.position.z < 0.9) {
-                obj.position.z += 0.06;
-            }
-
-            if (obj.rotation.z < -0.5) {
-                obj.rotation.z -= -0.022;
-            }
-
-            if (obj.position.y > -0.2) {
-                obj.position.y -= 0.05;
-            }
-        }
-
-        else if (active === 1 && obj.position.y === 0) {
-            if (obj.position.x < 0.05) {
-                obj.position.x += 0.05 / 30;
-            }
-
-            if (obj.position.z < 0.9) {
-                obj.position.z += 0.9 / 30;
-            }
-
-            if (obj.rotation.z < -0.5) {
-                obj.rotation.z -= -0.5 / 15;
-            }
-
-            if (obj.position.y < -0.2) {
-                obj.position.y += 0.2 / 30;
-            }
-            // obj.position.x = 0.05;
-            // obj.position.z = 0.9;
-            // obj.rotation.z = -0.5;
-        }
-    })
-
-    return (
-        <primitive
-            object={obj}
-            scale={3.5}
-            rotation={[
-                (-Math.PI / 180) * (90 - 1.333),
-                (-Math.PI / 180) * -0.1,
-                (-Math.PI / 180) * 90,
-            ]}
-        />
-    );
-};
+import Scene from "./Model/Scene";
 
 export default function App() {
     const { active: abc, progress } = useProgress();
